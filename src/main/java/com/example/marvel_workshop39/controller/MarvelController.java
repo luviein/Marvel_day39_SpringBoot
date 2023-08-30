fd7ef9a367e977071f424533a6428d205722781f
Yenleng <yenlengggggg@gmail.com>
2023-08-30 21:16:54 +0800

    add variables for deployment

--- src/main/java/com/example/marvel_workshop39/controller/MarvelController.java
+++ src/main/java/com/example/marvel_workshop39/controller/MarvelController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +27,7 @@ import jakarta.json.JsonObject;
 
 @RestController
 @RequestMapping(path = "/api")
-// @CrossOrigin
+@CrossOrigin
 public class MarvelController {
 
     @Autowired
